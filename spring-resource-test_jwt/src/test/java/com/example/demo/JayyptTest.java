411f4db069c9066ba87708c4596fe711e04d072c
재욱 <iuh12@naver.com>
2022-09-30 18:51:46 +0900

    docker 올리기는 끝

--- spring-resource-test_jwt/src/test/java/com/example/demo/JayyptTest.java
+++ spring-resource-test_jwt/src/test/java/com/example/demo/JayyptTest.java
@@ -9,17 +9,17 @@ public class JayyptTest {
 	 * @Test public void contextLoads() { }
 	 * 
 	 * @Test public void jasypt() { String url =
-	 * "jdbc:oracle:thin:@jakedbdb_high?TNS_ADMIN=./wallet";
-	 * 
-	 * 
-	 * System.out.println(jasyptEncoding(url));
+	 * "jdbc:oracle:thin:@(description= (retry_count=20)(retry_delay=3)(address=(protocol=tcps)(port=1521)(host=adb.ca-toronto-1.oraclecloud.com))(connect_data=(service_name=gcaa070203a63c7_jakedbdb_medium.adb.oraclecloud.com))(security=(ssl_server_dn_match=yes)))"
+	 * ;
+	 *  System.out.println(jasyptEncoding(url));
 	 * //System.out.println(jasyptEncoding(username));
-	 * //System.out.println(jasyptEncoding(password)); }
+	 * System.out.println(jasyptEncoding(password)); }
 	 * 
 	 * public String jasyptEncoding(String value) {
 	 * 
 	 * StandardPBEStringEncryptor pbeEnc = new StandardPBEStringEncryptor();
-	 * pbeEnc.setAlgorithm("PBEWithMD5AndDES"); // pbeEnc.setPassword(key); return
+	 * pbeEnc.setAlgorithm("PBEWithMD5AndDES"); // //pbeEnc.setPassword(key); return
 	 * pbeEnc.encrypt(value); }
 	 */
+
 }
